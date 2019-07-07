077543764008a70d7abc450af4b5fd46719bfab9
yamayuu <yamayuu.ics@gmail.com>
2019-07-07 05:13:49 +0100

    change pin number for raspi zero,indent

--- python/shutdown/shutdown.py
+++ python/shutdown/shutdown.py
@@ -5,31 +5,35 @@ import time
 import RPi.GPIO as GPIO
 import os
 
-pinnum=17
+pinnum=4
 
 GPIO.setmode(GPIO.BCM)
 
 
 GPIO.setup(pinnum,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
+#print('test')
+
 while True:
-	GPIO.wait_for_edge(pinnum,GPIO.FALLING)
-	sw_cnt = 0
+    GPIO.wait_for_edge(pinnum,GPIO.FALLING)
+    sw_cnt = 0
+    #print('tese2')
+    while True:
+        sw_status = GPIO.input(pinnum)
+        #print("in loop")
+        if sw_status == 0:
+            sw_cnt = sw_cnt + 1
+            if sw_cnt >= 50:
+                #print("Start Shutdown Sequence")
+		#os.system("echo "Start_Shutdown_Sequence"")
+                os.system("sudo shutdown -h now")
+                break
+            else:
+                pass
+                #print("Short push")
+                #break
 
-	while True:
-		sw_status = GPIO.input(pinnum)
-		if sw_status == 0:
-			sw_cnt = sw_cnt + 1
-			if sw_cnt >= 50:
-				#print("Start Shutdown Sequence")
-				#os.system("echo "Start_Shutdown_Sequence"")
-				os.system("sudo shutdown -h now")
-				break
-		else:
-			#print("Short push")
-			break
-
-		time.sleep(0.01)
+            time.sleep(0.01)
 
 	
 
